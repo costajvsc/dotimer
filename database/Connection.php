c0be1f3944fdaf855bc2fcd46997ecd24126ab3a
Joao Victor <Joao Victor@DESKTOP-MAD>
2021-05-11 06:14:25 -0300

    [DEV] - Create connection with MySQL

---
+++ database/Connection.php
@@ -0,0 +1,30 @@
+<?php
+
+class Connection 
+{
+    private $host = 'localhost';
+    private $username = 'root';
+    private $password = '';
+    private $database = 'dotimer';
+    private $connection = null;
+
+    public function __construct()
+    {
+        $this->startConnection();
+    }
+
+    private function startConnection()
+    {
+        $this->connection = mysqli_connection(
+            $this->host,
+            $this->username,
+            $this->password,
+            $this->database
+        );
+    }
+
+    public function getConnection()
+    {
+        return $this->connection;
+    }
+}
